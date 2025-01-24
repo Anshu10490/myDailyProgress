683093e0241487cd342bf8f62b281fe1fbd8201f
Anshu Kumari <anshu@Anshus-MacBook-Air.local>
2025-01-24 19:10:49 +0530

    done

--- Day1.java
+++ Day1.java
@@ -1,8 +1,5 @@
 class Day1{
     public static void main(String[] args) {
-        System.out.println("Raya");
-        System.out.println("hey there");
-        System.out.println("Kumari");
-        System.out.println("Anshu");
+       System.out.println("first repo");
     }
 }
