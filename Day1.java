da43bbef7571b34895ec85c904172d1602c3d344
Anshu Kumari <anshu@Anshus-MacBook-Air.local>
2025-01-24 18:50:23 +0530

    My second

--- Day1.java
+++ Day1.java
@@ -1,5 +1,7 @@
 class Day1{
     public static void main(String[] args) {
         System.out.println("Raya");
+        System.out.println("hey there");
+        System.out.println("Kumari");
     }
 }
