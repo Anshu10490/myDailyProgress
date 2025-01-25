8a7195541c8f578bb24d4b97d298ff7ff9323aee
Anshu Kumari <anshu@Anshus-MacBook-Air.local>
2025-01-25 16:14:17 +0530

    Rotate string

---
+++ RotateString.java
@@ -0,0 +1,9 @@
+public class RotateString {
+        public boolean rotateString(String s, String goal) {
+            if(s.length()!=goal.length()){
+                return false;
+            }
+            s=s+s;
+            return s.contains(goal);
+            }
+    }
